67449647352d95301370ab082f469abb0610d9f0
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2024-09-30 17:59:39 +0300

    revert

--- ServiceScan.SourceGenerator.Playground/ServiceCollectionExtensions.cs
+++ ServiceScan.SourceGenerator.Playground/ServiceCollectionExtensions.cs
@@ -5,8 +5,5 @@ namespace ServiceScan.SourceGenerator.Playground;
 public static partial class ServiceCollectionExtensions
 {
     [GenerateServiceRegistrations(AssignableTo = typeof(IService), TypeNameFilter = "*Ser*")]
-    public static partial IServiceCollection AddServices(this IServiceCollection services)
-    {
-        services.AddScoped(typeof(IService), typeof(IService));
-    }
+    public static partial IServiceCollection AddServices(this IServiceCollection services);
 }
