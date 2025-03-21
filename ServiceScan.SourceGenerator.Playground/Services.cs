bf8a7e153631800007265c5f2d2ce86157aac170
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2025-03-21 16:55:35 +0200

    revert playgound

--- ServiceScan.SourceGenerator.Playground/Services.cs
+++ ServiceScan.SourceGenerator.Playground/Services.cs
@@ -1,16 +1,6 @@
 ﻿namespace ServiceScan.SourceGenerator.Playground;
 
 public interface IService { }
-public class MyService1 : IService
-{
-    public const string Key = "Key";
-}
-public class MyService2 : IService
-{
-
-    public const string Key = "Key1";
-}
-public class MyServ111111111ice3 : IService
-{
-    public const string Key = "Key2";
-}
+public class MyService1 : IService { }
+public class MyService2 : IService { }
+public class MyServ111111111ice3 : IService { }
