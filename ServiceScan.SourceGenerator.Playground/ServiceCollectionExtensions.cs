4ef472a216ae55d9dbb6149ea61e4db7beed4874
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2025-10-01 18:26:30 +0300

    revert playground

--- ServiceScan.SourceGenerator.Playground/ServiceCollectionExtensions.cs
+++ ServiceScan.SourceGenerator.Playground/ServiceCollectionExtensions.cs
@@ -2,24 +2,8 @@
 
 namespace ServiceScan.SourceGenerator.Playground;
 
-public interface ICommandHandler<T> { }
-public class CommandHandlerDecorator<T>(ICommandHandler<T> inner) : ICommandHandler<T>;
-
-public class SpecificHandler1 : ICommandHandler<string>;
-public class SpecificHandler2 : ICommandHandler<long>;
-
 public static partial class ServiceCollectionExtensions
 {
-    [GenerateServiceRegistrations(AssignableTo = typeof(ICommandHandler<>), CustomHandler = nameof(AddDecoratedHandler))]
-    public static partial IServiceCollection AddHandlers(this IServiceCollection services);
-
-    private static void AddDecoratedHandler<THandler, TCommand>(this IServiceCollection services)
-        where THandler : class, ICommandHandler<TCommand>
-    {
-        // Add handler itself to DI
-        services.AddScoped<THandler>();
-
-        // Register decorated handler as ICommandHandler
-        services.AddScoped<ICommandHandler<TCommand>>(s => new CommandHandlerDecorator<TCommand>(s.GetRequiredService<THandler>()));
-    }
+    [GenerateServiceRegistrations(AssignableTo = typeof(IService), TypeNameFilter = "*Ser*")]
+    public static partial IServiceCollection AddServices(this IServiceCollection services);
 }
